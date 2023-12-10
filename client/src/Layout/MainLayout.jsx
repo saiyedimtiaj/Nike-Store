bd0961d0568d7c4168f1412941731adb83a167ea
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2023-12-10 11:56:09 +0600

    project setup

---
+++ client/src/Layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import { Nabvar } from "../Components/Nabvar/Nabvar";
+
+
+const MainLayout = () => {
+    return (
+        <div className="container mx-auto px-4">
+            <Nabvar/>
+        </div>
+    );
+};
+
+export default MainLayout;
